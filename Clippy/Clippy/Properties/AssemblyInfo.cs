cad56c56c6b0048fe43cbbe3495e6ee706167709
Josh <josh.quintus@gmail.com>
2015-07-26 17:45:46 -0400

    Include CoolWhip
    Adds build configuration for AppVeyor and instructions to publish package to NuGet.

--- Clippy/Clippy/Properties/AssemblyInfo.cs
+++ Clippy/Clippy/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Clippy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Treat the clipboard like a Stream")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MasterDevs")]
 [assembly: AssemblyProduct("Clippy")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
