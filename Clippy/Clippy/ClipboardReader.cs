40ca6cb69c141a0e722601a3468b9f3eb1b74568
Josh <josh.quintus@gmail.com>
2015-07-26 17:40:20 -0400

    Initial implemenation

---
+++ Clippy/Clippy/ClipboardReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Clippy
+{
+    public class ClipboardReader : ReadLineTextReader
+    {
+        private bool _isEof = false;
+
+        public override string ReadLine()
+        {
+            if (_isEof)
+            {
+                return null;
+            }
+            else
+            {
+                _isEof = true;
+
+                string clip = Clipboard.GetText();
+                return clip;
+            }
+        }
+    }
+}
