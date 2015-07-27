8c6289f51959845b93fdf0911d270afc3a77e0ea
Josh <josh.quintus@gmail.com>
2015-07-27 10:05:20 -0400

    Update AssemblyInfo.cs

--- Clippy/Clippy/Properties/AssemblyInfo.cs
+++ Clippy/Clippy/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MasterDevs.Clippy")]
-[assembly: AssemblyDescription("Treat the clipboard like a Stream")]
+[assembly: AssemblyDescription("Treat the clipboard like a TextReader and TextWriter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MasterDevs")]
 [assembly: AssemblyProduct("MasterDevs.Clippy")]
